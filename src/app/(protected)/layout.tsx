c4f04a34b12ffbb1daabbeb8be9d9f2089fe2bf6
global-ghost <ghost.of.tandem@gmail.com>
2025-10-05 18:39:10 +0300

    some ui fixes

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -17,7 +17,7 @@ export default async function ProtectedLayout({
     return null;
   }
   return (
-    <div className='flex min-h-screen pb-[50px] md:pb-0'>
+    <div className='flex min-h-screen pb-[70px] md:pb-0'>
       <div className='hidden w-full max-w-[60px] flex-col bg-navbar px-[10px] md:flex'>
         <div className='flex justify-center py-8'>
           <Image src={logo} alt='logo' height={24} priority />
@@ -41,41 +41,38 @@ export default async function ProtectedLayout({
       </div>
 
       <div className='fixed bottom-0 z-10 flex w-full items-center bg-navbar px-[10px] md:hidden'>
-        <nav className='mt-2 flex size-full items-center justify-center gap-7 py-2 pl-4 text-center'>
+        <nav className='mt-2 flex size-full items-center justify-center gap-6 text-center '>
+          <Image src={logo} alt='logo' height={26} priority />
+          <div className='grow' />
           <Link className='block' href='/map'>
-            <Icon icon='location' size={28} />
+            <Icon icon='location' size={26} />
           </Link>
           <Link className='block' href='/contacts'>
-            <Icon icon='group' size={28} />
+            <Icon icon='group' size={26} />
           </Link>
 
           <Link className='block' href='/settings'>
-            <Icon icon='settings' size={28} />
+            <Icon icon='settings' size={26} />
           </Link>
 
           <div className='grow' />
 
-          <SignOutButton size={28} />
+          <div className='pb-1'>
+            <SignOutButton size={26} />
+          </div>
         </nav>
-      </div>
-
-      <div
-        className='fixed left-3 top-2 z-10 flex size-12 items-center justify-center rounded-full
-          border border-cardBorder bg-background p-1 md:hidden'
-      >
-        <Image src={logo} alt='logo' height={24} priority />
       </div>
 
       <div className='flex w-full flex-col'>
         {/* <header className='h-20 w-full bg-card py-8 pl-4'>Header</header> */}
 
-        <main className='m-[7px] grow py-2 sm:px-1 lg:m-4 lg:p-2'>
+        <main className='m-[3px] grow py-1 sm:px-1 lg:m-4 lg:p-2'>
           <SessionProvider>
             <MapProvider>{children}</MapProvider>
           </SessionProvider>
         </main>
 
-        <footer className='static mt-8 h-20 w-full py-8 pl-4 text-center text-xs sm:text-sm'>
+        <footer className='static h-20 w-full py-8 pl-4 text-center text-xs sm:text-sm'>
           @ 2025 Tamdemeet. All right reserved.
         </footer>
       </div>
